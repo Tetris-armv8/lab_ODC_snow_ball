aad3758973479701794ad673d30e7db3beb20069
alexis <alexis@debian.debianvisita>
2023-05-18 19:43:53 -0300

    Lab agregado

---
+++ snow/rpi-asm-framebuffer/datos.s
@@ -0,0 +1,42 @@
+
+
+//Colores: 
+
+// Fondo
+colorCielo: .word 0xd6ebef          // Celeste
+colorNieveQuecae: .word 0xeff7f7    // Celeste claro  
+colorNieve : .word 0xffffff  	    // Blanco
+
+
+//Arbol
+colorDeTroncoArbol: .word 0x7b554a
+colorDeHojasArbol : .word 0x387d29
+
+
+
+//bola de nieve
+colorBrazosBolaNieve: .word 0x7b554a //Marron
+colorOjosBolaNieve: .word  0x212421 //Negro
+colorBocaBolaNieve: .word  0x212421 //Negro
+
+//angelito 
+colorAdentroAngel 0xe7f3f7 
+colorBordeAngel  0xd6e7ef
+
+//muñecohuevo
+colorDelHuevo : .word 0xffcb42  //Amarillo
+colorSombraIzqHuevo: .word 0xf7b500 //Amarillosombreado
+colorSombraArribaHuevo: .word 0xcf8613 //Amarillosombreado oscuro
+colorOjosHuevo: .word  0x212421 //Negro
+colorBocaHuevo: .word 0x212421  //Negro
+colorDoblezGorroHuevo: .word 0x385bac //Azul oscuro
+colorPomponHuevo: .word 0x667dbd   //Azul
+colorCumpulaGorroHuevo: .word 0x667dbd //Azul
+colorSombraPomponGorroHuevo: .word 0x385bac //Azul oscuro
+colorBocaHuevoAdentro: .word  0xe74c3c //Rojo
+
+
+
+
+
+
